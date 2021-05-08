fb2374965f5028217ffbc09dc3dafefdbabd742c
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-05-08 13:53:45 +0200

    scripts

--- apartado1/script_medicion_apartado1.sh
+++ apartado1/script_medicion_apartado1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-make # Compilamos
+make -f Makefile_O0 clean # Compilamos
+make -f Makefile_O0 # Compilamos
 rm medidas_1.csv # Limpiamos el fichero de resultados
 echo 'ID_PRUEBA,N,T_TOTAL' > medidas_1.csv # Cabecera del fichero
 lista_n="250 500 750 1000 1500 2000 2550 3000" # Valores de C para probar
@@ -12,3 +13,23 @@ do
         eval "./p2_apartado1.out $n $id_prueba" # Ejecutamos el primer experimento
     done
 done
+
+mv medidas_1.csv medidas_1_O0.csv
+
+#!/bin/bash
+make -f Makefile_O2 clean # Compilamos
+make -f Makefile_O2 # Compilamos
+rm medidas_1.csv # Limpiamos el fichero de resultados
+echo 'ID_PRUEBA,N,T_TOTAL' > medidas_1.csv # Cabecera del fichero
+lista_n="250 500 750 1000 1500 2000 2550 3000" # Valores de C para probar
+for id_prueba in {1..20}; # Repetimos las pruebas 50 veces
+do
+    echo "=========== ID DE PRUEBA: $id_prueba ==========="
+    for n in $lista_n; # Para cada valor de C...
+    do
+        echo "Ejecutando prueba con N=$n" # Informacion por pantalla
+        eval "./p2_apartado1.out $n $id_prueba" # Ejecutamos el primer experimento
+    done
+done
+
+mv medidas_1.csv medidas_1_O2.csv
