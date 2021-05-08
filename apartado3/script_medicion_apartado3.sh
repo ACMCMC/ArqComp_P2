612454e0c173aac74b7509845df207b9f66c2273
ACMCMC <aldan.creo@gmail.com>
2021-05-08 17:50:56 +0200

    medidas parte 3

--- apartado3/script_medicion_apartado3.sh
+++ apartado3/script_medicion_apartado3.sh
@@ -3,7 +3,7 @@ make # Compilamos
 rm medidas_3.csv # Limpiamos el fichero de resultados
 echo 'ID_PRUEBA,N,T_TOTAL' > medidas_3.csv # Cabecera del fichero
 lista_n="250 500 750 1000 1500 2000 2550 3000" # Valores de C para probar
-for id_prueba in {1..50}; # Repetimos las pruebas 50 veces
+for id_prueba in {1..20}; # Repetimos las pruebas 50 veces
 do
     echo "=========== ID DE PRUEBA: $id_prueba ==========="
     for n in $lista_n; # Para cada valor de C...
