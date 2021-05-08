3c111b6508fb6e139951907405bdc29e0a1b2151
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-05-08 22:18:43 +0200

    script

--- apartado4/script_medicion_apartado4.sh
+++ apartado4/script_medicion_apartado4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 make # Compilamos
-rm medidas_4.csv # Limpiamos el fichero de resultados
-echo 'ID_PRUEBA,N,T_TOTAL' > medidas_4.csv # Cabecera del fichero
 lista_n="250 500 750 1000 1500 2000 2550 3000" # Valores de C para probar
 lista_num_threads="1 2 4 6 8" # Valores de C para probar
 for num_threads in $lista_num_threads;
 do
+    rm medidas_4.csv # Limpiamos el fichero de resultados
+    echo 'ID_PRUEBA,N,T_TOTAL' > medidas_4.csv # Cabecera del fichero
     export OMP_NUM_THREADS=$num_threads
     echo "=========== NUMERO DE HILOS: $OMP_NUM_THREADS ==========="
     for id_prueba in {1..20}; # Repetimos las pruebas 50 veces
@@ -17,4 +17,5 @@ do
             eval "./p2_apartado4.out $n $id_prueba" # Ejecutamos el primer experimento
         done
     done
+    mv medidas_4.csv "medidas_4_$num_threads.csv"
 done
