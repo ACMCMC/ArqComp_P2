ab8635be10d2bbaed8def09767325b51c46037f0
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-05-12 21:57:53 +0200

    scripts

---
+++ run_all_tests.sh
@@ -0,0 +1,4 @@
+source ./apartado1/script_medicion_apartado1.sh
+source ./apartado1/script_medicion_apartado2.sh
+source ./apartado1/script_medicion_apartado3.sh
+source ./apartado1/script_medicion_apartado4.sh
