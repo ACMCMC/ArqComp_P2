5eea714c179e06cbbc754df72e9afb79aa7262af
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-05-12 21:59:44 +0200

    script

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,4 +1,8 @@
-source ./apartado1/script_medicion_apartado1.sh
-source ./apartado1/script_medicion_apartado2.sh
-source ./apartado1/script_medicion_apartado3.sh
-source ./apartado1/script_medicion_apartado4.sh
+cd apartado1
+/bin/bash script_medicion_apartado1.sh
+cd ../apartado2
+/bin/bash script_medicion_apartado2.sh
+cd ../apartado3
+/bin/bash script_medicion_apartado3.sh
+cd ../apartado4
+/bin/bash script_medicion_apartado4.sh
